e4a81bb503f975a90f0b09f1bfd9e6bc416f7d47
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2022-07-02 02:44:58 +0600

    cors error solving

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const app = express()
 app.use(cors());
 app.use(express.json())
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+  })
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ztdf7.mongodb.net/?retryWrites=true&w=majority`;
